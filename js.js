e03ee4549a06e9089de240322ef36a0382055643
love sonkar <loveboss806@gmail.com>
2022-09-04 15:05:11 +0530

    added eye feature in password section

--- js.js
+++ js.js
@@ -1,6 +1,16 @@
+// making variable for inputs
+
 let user = document.querySelector(".user");
 let pass = document.querySelector(".pass");
 let btn = document.querySelector(".submit");
+
+// user name and pass store in a variable
+
+let userid = "love";
+let userpass = "123456";
+
+// success box code start
+
 let msg = document.querySelector(".success-msg");
 let close = document.querySelector(".ri-close-line");
 
@@ -10,8 +20,28 @@ close.addEventListener("click", () => {
   msg.style.display = "none";
 });
 
-let userid = "love";
-let userpass = "123456";
+// end success box code
+
+// password eye option
+let eyeMenu = document.querySelector(".eye");
+let eyeOpen = document.querySelector(".ri-eye-fill");
+let eyeClose = document.querySelector(".ri-eye-off-fill");
+
+eyeOpen.style.display = "none";
+
+// making a click event for eye function
+
+eyeMenu.addEventListener("click", () => {
+  eyeClose.style.display === "block" || eyeOpen.style.display === "none"
+    ? ((eyeClose.style.display = "none"),
+      (eyeOpen.style.display = "block"),
+      (pass.type = "text"))
+    : ((eyeClose.style.display = "block"),
+      (eyeOpen.style.display = "none"),
+      (pass.type = "password"));
+});
+
+// scripting start here for validation
 
 btn.addEventListener("click", (e) => {
   let valpass = pass.value;
@@ -21,6 +51,8 @@ btn.addEventListener("click", (e) => {
     errorColor();
   } else {
   }
+
+  // making function for error code
 
   function errorColor() {
     let userinput = document.querySelector(".userinput");
@@ -37,6 +69,8 @@ btn.addEventListener("click", (e) => {
     usericon.style.color = "red";
     passicon.style.color = "red";
   }
+
+  // end function here
 
   function logIn() {
     if (valuser != userid || valpass != userpass) {
